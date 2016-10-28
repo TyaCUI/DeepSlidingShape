beda8881ee81982c7c4b983251781db0c7549ad4
TyaCUI <TyaCUI@users.noreply.github.com>
2016-10-28 17:18:36 +0800

    Add files via upload

---
+++ code/matlab_code/fetchdata.m
@@ -0,0 +1,4 @@
+downloadData('../sunrgbd_dss_data','http://dss.cs.princeton.edu/Release/sunrgbd_dss_data/','.bin');
+downloadData('../image','http://dss.cs.princeton.edu/Release/image/','.tensor');
+downloadData('../proposal','http://dss.cs.princeton.edu/Release/result/proposal/RPN_SUNRGBD/','.mat');
+downloadData('../seg','http://dss.cs.princeton.edu/Release/seg/','.mat');
